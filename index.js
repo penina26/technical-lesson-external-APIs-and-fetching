8b2738d32599b8abb0dc6677b77a7450bc85e1ca
Penina <wpenina@gmail.com>
2025-12-25 13:20:04 +0300

    Fetch and display astrounats from API

--- index.js
+++ index.js
@@ -12,3 +12,25 @@
 // Hint: Use .then() to process the response and convert it to JSON
 // Hint: Call the displayAstronauts() function with the fetched data
 // Hint: Use .catch() to handle any errors that occur during the fetch process
+
+
+function displayAstronauts(data) {
+    const astronautList = document.getElementById("astronaut-list");
+    data.people.forEach(person => {
+        const listItem = document.createElement("li");
+        listItem.textContent = person.name;
+        astronautList.appendChild(listItem);
+    });
+}
+
+fetch('http://api.open-notify.org/astros.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        displayAstronauts(data);
+    })
+    .catch(function (error) {
+        console.log("Error fetching data:", error);
+    });
+
